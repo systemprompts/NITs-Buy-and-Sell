9d902a3377ce96960c441a6eeac5e930a8cda48a
systemprompts <systemprompts488@gmail.com>
2026-09-02 18:06:03 +0600

    Safeguard user_loader against DB connection errors

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -9,4 +9,8 @@ from app.models.item import Item, ItemImage, ItemContact, Comment
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    try:
+        return User.query.get(int(user_id))
+    except Exception:
+        return None
+
